58fa9efed2fa3c3b7445501fb658a58e914478c6
Mostafizur-Pro <mostafizur0195@gmail.com>
2024-01-01 21:04:11 +0600

    Wishlist add

---
+++ src/components/wishlist/NavLinks.tsx
@@ -0,0 +1,106 @@
+import React, { useState } from "react";
+import  Link  from "next/link";
+import { links } from "./links";
+
+const NavLinks = () => {
+  const [heading, setHeading] = useState("");
+  const [subHeading, setSubHeading] = useState("");
+  return (
+    <>
+      {links.map((link) => (
+        <div>
+          <div className="px-3 text-left cursor-pointer group">
+            <h1
+              className="py-1 flex justify-between items-center  pr-5 group"
+              onClick={() => {
+                heading !== link.name ? setHeading(link.name) : setHeading("");
+                setSubHeading("");
+              }}
+            >
+              {link.name}
+              <span className="text-xl md: hidden ">
+
+              </span>
+              <span className="text-xl hidden group-hover:rotate-180 group-hover:-mt-2">
+
+
+              </span>
+            </h1>
+            {link.submenu && (
+              <div>
+                <div className="absolute top-20 hidden ">
+                  <div className="py-3">
+                    <div
+                      className="w-4 h-4 left-3 absolute 
+                    mt-1 bg-white rotate-45"
+                    ></div>
+                  </div>
+                  <div className="bg-white p-5 grid grid-cols-3 gap-10">
+                    {link.sublinks.map((mysublinks) => (
+                      <div>
+                        <h1 className="text-lg font-semibold">
+                          {mysublinks.Head}
+                        </h1>
+                        {mysublinks.sublink.map((slink) => (
+                          <li className="text-sm text-gray-600 my-2.5">
+                            <Link
+                              href={slink.link}
+                              className="hover:text-primary"
+                            >
+                              {slink.name}
+                            </Link>
+                          </li>
+                        ))}
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              </div>
+            )}
+          </div>
+          {/* Mobile menus */}
+          <div
+            className={`
+            ${heading === link.name ? "" : "hidden"}
+          `}
+          >
+            {/* sublinks */}
+            {link.sublinks.map((slinks) => (
+              <div>
+                <div>
+                  <h1
+                    onClick={() =>
+                      subHeading !== slinks.Head
+                        ? setSubHeading(slinks.Head)
+                        : setSubHeading("")
+                    }
+                    className="py-4 pl-7 font-semibold flex justify-between items-center md:pr-0 pr-5"
+                  >
+                    {slinks.Head}
+
+                    <span className="text-xl  inline">
+
+                    </span>
+                  </h1>
+                  <div
+                    className={`${
+                      subHeading === slinks.Head ? "" : "hidden"
+                    }`}
+                  >
+                    {slinks.sublink.map((slink) => (
+                      <li className="py-3 pl-14">
+                        <Link href={slink.link}>{slink.name}</Link>
+                      </li>
+                    ))}
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default NavLinks;
---
+++ src/pages/wishlist/index.tsx
@@ -0,0 +1,80 @@
+import Image from "next/image";
+// import img1 from "../../../public/9_thumb.jpg";
+import img1 from "../../assets/wishlist/9_thumb.jpg";
+import React from "react";
+import Link from "next/link";
+import Sidebar from "@/components/wishlist/sidebar";
+import WishlistBanner from "@/components/wishlist/WishlistBanner";
+
+
+
+
+
+const WishList = () => {
+  return (
+    <div>
+      <WishlistBanner/>
+      <section className="bg-slate-50 md:grid-cols-3 px-3 md:px-0 grid gap-5 max-w-7xl mx-auto my-10">
+      {/* nav section */}
+      <div className="md:col-span-1 border">
+        <Sidebar/>
+      </div>
+      {/* product section container */}
+      <div className="md:col-span-2 border">
+        <h1 className="uppercase">Your WishList</h1>
+        <div className="mt-3 pt-10 bg-white">
+          <table className=" w-full">
+            <thead className="">
+              <tr className="">
+                <th></th>
+                <th className="text-left">DESCRIPTION</th>
+                <th className="text-left">ID</th>
+                <th className="text-left">Unit Price</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr className="border-t border-red-400">
+                <td className="py-[30px] px-[8px] ">
+                  <Image src={img1} alt="img1"></Image>
+                </td>
+                <td>
+                  <p className="uppercase text-red-400 text-sm opacity-90">
+                    <Link href={"/"}>Black T-Shirt</Link>
+                  </p>
+                  <p className="text-gray-400 text-xs">
+                    Color: <span className="italic">Black</span>, Size:{" "}
+                    <span className="italic">M</span>
+                  </p>
+                </td>
+                <td>SK169841669</td>
+                <td>$14.63</td>
+                <td className="text-red-500 text-xl cursor-pointer">x</td>
+              </tr>
+              <tr className="border-t">
+                <td className="py-[30px] px-[8px] ">
+                  <Image src={img1} alt="img1"></Image>
+                </td>
+                <td>
+                  <p className="uppercase text-red-400 text-sm opacity-90">
+                    <Link href={"/"}>Black T-Shirt</Link>
+                  </p>
+                  <p className="text-gray-400 text-xs">
+                    Color: <span className="italic">Black</span>, Size:{" "}
+                    <span className="italic">M</span>
+                  </p>
+                </td>
+                <td>SK169841669</td>
+                <td>$14.63</td>
+                <td className="text-red-500 text-xl cursor-pointer">x</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </section>
+    </div>
+  );
+};
+
+export default WishList;
