7af3262069ef5a846e48c81a9b3ed62f78a682d4
Mostafizur-Pro <mostafizur0195@gmail.com>
2024-01-01 20:34:27 +0600

    product details page update

---
+++ src/components/ProductDetailsPage/ProductDetailsSidebar.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProductDetailsSidebar = () => {
+    return (
+        <div>
+             <h2 className="">SideBar</h2>
+        </div>
+    );
+};
+
+export default ProductDetailsSidebar;
