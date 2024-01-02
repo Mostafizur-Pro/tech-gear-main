dac22f926332e430f293ca34baa85c404529dcd0
SabbirSami <smd71430@gmail.com>
2024-01-02 14:58:13 +0600

    add banner section

--- src/components/HomePage/Sliderbar.tsx
+++ src/components/HomePage/Sliderbar.tsx
@@ -1,115 +1,48 @@
-import React from 'react';
+import Image from "next/image";
+import bannerImage01 from "../../assets/bannerImage01.jpg";
+import bannerImage02 from "../../assets/bannerImage02.jpg";
+import { HiArrowLongRight } from "react-icons/hi2";
 
 const Sidebar = () => {
     return (
-        <div>
-            <div className="carousel w-full">
-                <div id="slide1" className="carousel-item relative w-full">
-                    <div className='sm:grid bg-rose-200 grid-cols-2'>
-                        <div className=' '>
-
-                            <div className='px-8 py-4'>
-                                <div className='ps-12 pt-12 flex items-center justify-center'>
-                                    <div className=' '>
-                                        <h1 className='text-gray-700 text-xl font-medium'>50% off in all products</h1>
-                                        <h1 className='text-gray-700 text-3xl font-bold'>Summer Sale</h1>
-                                        <button className="btn btn-active btn-md bg-rose-400 text-white my-4 text-xm font-bold">Buy Now</button>
-                                    </div>
-                                </div>
-
-
-                            </div>
-                        </div>
-                        <div className='flex items-center justify-center '>
-                            <img src="https://daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.jpg" className="w-[75%]   h-full rounded-lg " />
-                        </div>
-
-                    </div>
-                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                        <a href="#slide4" className="btn btn-circle bg-rose-400 text-white">❮</a>
-                        <a href="#slide2" className="btn btn-circle bg-rose-400 text-white">❯</a>
-                    </div>
+        <section className="container mx-auto px-6">
+            <div className="grid grid-cols-7 items-center pt-44">
+                <div className="col-span-3">
+                    <h2 className="text-8xl uppercase font-semibold">
+                        Flash Sale Big SAle
+                    </h2>
+                    <p className=" py-6 ps-2 pe-10 opacity-75 font-medium">
+                        Discover the latest trends in fashion at our online
+                        clothing store! Elevate your style with our curated
+                        collection of chic and comfortable clothing. From
+                        timeless classics to on-trend pieces, find the perfect
+                        outfit for any occasion.
+                    </p>
+                    <button className=" rounded-full border-2 font-semibold px-6 py-3 ">
+                        Shop Now{" "}
+                        <HiArrowLongRight className=" inline text-2xl ms-2" />
+                    </button>
                 </div>
-                <div id="slide2" className="carousel-item relative w-full">
-                    <div className='sm:grid bg-rose-200 grid-cols-2'>
-                        <div className=' '>
-
-                            <div className='px-8 py-4'>
-                                <div className='ps-12 pt-12 flex items-center justify-center'>
-                                    <div className=' '>
-                                        <h1 className='text-gray-700 text-xl font-medium'>50% off in all products</h1>
-                                        <h1 className='text-gray-700 text-3xl font-bold'>Summer Sale</h1>
-                                        <button className="btn btn-active btn-md bg-rose-400 text-white my-4 text-xm font-bold">Buy Now</button>
-                                    </div>
-                                </div>
-
-
-                            </div>
-                        </div>
-                        <div className='flex items-center justify-center '>
-                            <img src="https://daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.jpg" className="w-[75%]   h-full rounded-lg " />
-                        </div>
-
+                <div className="flex gap-10 col-span-4">
+                    <div className="relative">
+                        <Image
+                            className="rounded-t-full w-[45rem]"
+                            src={bannerImage01}
+                            alt="Banner Image"
+                        ></Image>
+                        <div className="w-[27rem] h-[27rem] border  border-black absolute -top-3 rounded-full -left-3"></div>
                     </div>
-                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                        <a href="#slide1" className="btn btn-circle bg-rose-400 text-white">❮</a>
-                        <a href="#slide3" className="btn btn-circle bg-rose-400 text-white">❯</a>
-                    </div>
-                </div>
-                <div id="slide3" className="carousel-item relative w-full">
-                    <div className='sm:grid bg-rose-200 grid-cols-2'>
-                        <div className=' '>
 
-                            <div className='px-8 py-4'>
-                                <div className='ps-12 pt-12 flex items-center justify-center'>
-                                    <div className=' '>
-                                        <h1 className='text-gray-700 text-xl font-medium'>50% off in all products</h1>
-                                        <h1 className='text-gray-700 text-3xl font-bold'>Summer Sale</h1>
-                                        <button className="btn btn-active btn-md bg-rose-400 text-white my-4 text-xm font-bold">Buy Now</button>
-                                    </div>
-                                </div>
-
-
-                            </div>
-                        </div>
-                        <div className='flex items-center justify-center '>
-                            <img src="https://daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.jpg" className="w-[75%]   h-full rounded-lg " />
-                        </div>
-
-                    </div>
-                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                        <a href="#slide2" className="btn btn-circle bg-rose-400 text-white">❮</a>
-                        <a href="#slide4" className="btn btn-circle bg-rose-400 text-white">❯</a>
-                    </div>
-                </div>
-                <div id="slide4" className="carousel-item relative w-full">
-                    <div className='sm:grid bg-rose-200 grid-cols-2'>
-                        <div className=' '>
-
-                            <div className='px-8 py-4'>
-                                <div className='ps-12 pt-12 flex items-center justify-center'>
-                                    <div className=' '>
-                                        <h1 className='text-gray-700 text-xl font-medium'>50% off in all products</h1>
-                                        <h1 className='text-gray-700 text-3xl font-bold'>Summer Sale</h1>
-                                        <button className="btn btn-active btn-md bg-rose-400 text-white my-4 text-xm font-bold">Buy Now</button>
-                                    </div>
-                                </div>
-
-
-                            </div>
-                        </div>
-                        <div className='flex items-center justify-center '>
-                            <img src="https://daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.jpg" className="w-[75%]   h-full rounded-lg " />
-                        </div>
-
-                    </div>
-                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                        <a href="#slide3" className="btn btn-circle bg-rose-400 text-white">❮</a>
-                        <a href="#slide1" className="btn btn-circle bg-rose-400 text-white">❯</a>
+                    <div className="">
+                        <Image
+                            className="rounded-t-full w-[45rem]"
+                            src={bannerImage02}
+                            alt="Banner Image 02"
+                        ></Image>
                     </div>
                 </div>
             </div>
-        </div>
+        </section>
     );
 };
 
