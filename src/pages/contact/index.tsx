0ccfc41fae8f5b57f0d16d9022abd80dc1f15d96
mostafizur-pro <mostafizur0195@gmail.com>
2024-01-08 11:31:12 +0600

    contact page banner add

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -5,16 +5,19 @@ import OthersCompanyLogo from "@/components/Shared/OthersCompany";
 
 // Import Swiper styles
 import "swiper/css";
+import ContactBanner from "@/components/ContactPage/ContactBanner";
 
 const ContactPage = () => {
- 
   const [agreed, setAgreed] = useState(false);
   return (
-   <div>
-    <HeadQuarters/>
-    <HelpLine/>
-    <OthersCompanyLogo/>
-   </div>
+    <div>
+      <ContactBanner />
+      <div className="mx-10">
+        <HeadQuarters />
+      </div>
+      <HelpLine />
+      <OthersCompanyLogo />
+    </div>
   );
 };
 
