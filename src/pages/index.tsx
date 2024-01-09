f79f7df537985294f8d9a4b46bf041b6badfe6b5
SabbirSami <smd71430@gmail.com>
2024-01-09 21:21:39 +0600

    make testimonial section responsive

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import Collection from "@/components/HomePage/Collection";
 import HeroSection from "@/components/HomePage/HeroSection";
 import NewArrival from "@/components/HomePage/NewArrival";
 import Newsletter from "@/components/HomePage/Newsletter";
+import Testimonials from "@/components/HomePage/Testimonial";
 import Product from "@/components/Product/Product";
 
 const Home = () => {
@@ -11,6 +12,7 @@ const Home = () => {
             <NewArrival />
             <Collection />
             <Newsletter />
+            <Testimonials />
             <Product />
         </div>
     );
